17398a6624d45b06aff2bd68a0545ebca043afd7
neversaynever44 <Max>
2017-08-18 16:24:59 +0300

    start

--- src/js/common.js
+++ src/js/common.js
@@ -1,39 +1,37 @@
 $(document).ready(function() {
 // slider ingridients
-	$('.js-slider2').owlCarousel({
-		autoplay: true,
-		autoplayTimeout: 6000,
-		center: true,
-		loop: true,
-		smartSpeed: 550,
-		slideSpeed: 500,
-		nav: false,
-		// stagePadding: 30,	
-		// margin: 30,
-    items: 5,
-    mouseDrag: false,
-    touchDrag: true,
-    onInitialized: carouselInitialized2,
-    onTranslate: carouselInitialized2,
-    responsive:{
-    	0:{
-    		items:1,
-    		autoWidth: true,
-    		nav: true,
-		    navContainerClass: 'customNav',
-		    navText: [$('.js-prev'),$('.js-next')],
-    	},
-    	480:{
-    		items:3,
-    	},
-    	767:{
-    		items:3,
-    	},
-    	768:{
-    		items:5,
-    	}
-    }
-  }).on('click', '.owl-item', function(e) {
+$('.js-slider2').owlCarousel({
+	autoplay: true,
+	autoplayTimeout: 6000,
+	center: true,
+	loop: true,
+	smartSpeed: 550,
+	slideSpeed: 500,
+	nav: false,
+	items: 5,
+	mouseDrag: false,
+	touchDrag: true,
+	onInitialized: carouselInitialized2,
+	onTranslate: carouselInitialized2,
+	responsive:{
+		0:{
+			items:1,
+			autoWidth: true,
+			nav: true,
+			navContainerClass: 'customNav',
+			navText: [$('.js-prev'),$('.js-next')],
+		},
+		480:{
+			items:3,
+		},
+		767:{
+			items:3,
+		},
+		768:{
+			items:5,
+		}
+	}
+	}).on('click', '.owl-item', function(e) {
 		var carousel = $('.js-slider2').data('owl.carousel');
 		e.preventDefault();
 		carousel.to(carousel.relative($(this).index()));
